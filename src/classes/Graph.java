59df9cbe0439baf70e3c885c33b130499adb650f
ahmed127011 <ahmed127011@gmail.com>
2018-05-14 00:35:07 +0200

    updating the graph logic to get 2 loops with the same indexes

--- src/classes/Graph.java
+++ src/classes/Graph.java
@@ -6,20 +6,21 @@ import java.util.Stack;
 import interfaces.IGraph;
 import interfaces.INode;
 
-public class Graph  implements IGraph   {
+public class Graph implements IGraph {
 	private int n;
 	private INode[] nodes;
 	private ArrayList<Path> loops;
-	private int delta=1;
+	private int delta = 1;
 	private double overallGain;
 	private ArrayList<Path> forwardpathes;
 	private ArrayList<Path> nonTouchedLoops;
-	private int[]nodesId;
+	private int[] nodesId;
 
-	public Graph(int numberOfnodes, int[] pathStart, int[] pathEnd, int[] pathGain, int startNode, int endNode,int[] nodesid) {
+	public Graph(int numberOfnodes, int[] pathStart, int[] pathEnd, int[] pathGain, int startNode, int endNode,
+			int[] nodesid) {
 		this.n = numberOfnodes;
 		nodes = new Node[n];
-		this.nodesId=nodesid;
+		this.nodesId = nodesid;
 		for (int i = 0; i < n; i++) {
 			nodes[i] = new Node(numberOfnodes);
 		}
@@ -29,41 +30,34 @@ public class Graph  implements IGraph   {
 		generateNontouchedLoops();
 	}
 
-	public String getCalculations()
-	{
-		StringBuilder result= new StringBuilder("delta = ");
-		result.append(delta+" \n");
-		for(int i=0;i<forwardpathes.size();i++)
-		{
-			int temp=getDelta(forwardpathes.get(i));
-			overallGain+=temp*forwardpathes.get(i).getGainInt();
-			result.append("delta"+(i+1)+"= "+temp+"\n");
+	public String getCalculations() {
+		StringBuilder result = new StringBuilder("delta = ");
+		result.append(delta + " \n");
+		for (int i = 0; i < forwardpathes.size(); i++) {
+			int temp = getDelta(forwardpathes.get(i));
+			overallGain += temp * forwardpathes.get(i).getGainInt();
+			result.append("delta" + (i + 1) + "= " + temp + "\n");
 		}
-		overallGain/=delta;
-		result.append("the Overall transfer Function ="+overallGain);
-		
+		overallGain /= delta;
+		result.append("the Overall transfer Function =" + overallGain);
+
 		return result.toString();
 	}
+
 	private int getDelta(Path path) {
-		int delta=1;
-		for(Path loop:loops)
-		{
-			if(loop.isAdditive(path.getLoops(), path.getBitnodes()))
-			{
-				delta-=loop.getGainInt();
+		int delta = 1;
+		for (Path loop : loops) {
+			if (loop.isAdditive(path.getLoops(), path.getBitnodes())) {
+				delta -= loop.getGainInt();
 			}
-			
+
 		}
-		for(Path loop:nonTouchedLoops)
-		{
-			if(loop.isAdditive(path.getLoops(), path.getBitnodes()))
-			{
-				if(loop.getBitnodes().size()%2==0)
-				{
-					delta+=loop.getGainInt();
-				}
-				else
-					delta-=loop.getGainInt();
+		for (Path loop : nonTouchedLoops) {
+			if (loop.isAdditive(path.getLoops(), path.getBitnodes())) {
+				if (loop.getBitnodes().size() % 2 == 0) {
+					delta += loop.getGainInt();
+				} else
+					delta -= loop.getGainInt();
 			}
 		}
 		return delta;
@@ -78,7 +72,7 @@ public class Graph  implements IGraph   {
 			result.append("P" + i + ": (");
 			for (int j = 0; j < loop.getLoop().length; j++) {
 				if (j == loop.getLoop().length - 1)
-					result.append(nodesId[loop.getLoop()[j]] + ") Gain= "+loop.getGainInt()+"\n");
+					result.append(nodesId[loop.getLoop()[j]] + ") Gain= " + loop.getGainInt() + "\n");
 				else
 					result.append(nodesId[loop.getLoop()[j]] + ", ");
 
@@ -89,24 +83,21 @@ public class Graph  implements IGraph   {
 		return result.toString();
 	}
 
-	public String getNonTouchedLoops()
-	{
+	public String getNonTouchedLoops() {
 		StringBuilder result = new StringBuilder("Non-Touched Loops:\n");
 		ArrayList<Path> now = new ArrayList<>();
 		now = getnontouched(2);
-		int c=2;
-		while (now.size() != 0) 
-		{
-			result.append(""+c+" non-Touched Loops: \n\t");
-			for (int i = 0;i<now.size(); i++)
-			{
+		int c = 2;
+		while (now.size() != 0) {
+			result.append("" + c + " non-Touched Loops: \n\t");
+			for (int i = 0; i < now.size(); i++) {
 				result.append("(");
-				for (int j = 0; j < now.get(i).getLoopIndex().size(); j++) 
-				{
+				for (int j = 0; j < now.get(i).getLoopIndex().size(); j++) {
 					if (j == now.get(i).getLoopIndex().size() - 1)
-						result.append("P"+(now.get(i).getLoopIndex().get(j)+1) + ") Gain="+now.get(i).getGainInt());
+						result.append(
+								"P" + (now.get(i).getLoopIndex().get(j) + 1) + ") Gain=" + now.get(i).getGainInt());
 					else
-						result.append("P"+(now.get(i).getLoopIndex().get(j)+1) + ", ");
+						result.append("P" + (now.get(i).getLoopIndex().get(j) + 1) + ", ");
 
 				}
 				result.append("\n\t");
@@ -138,7 +129,7 @@ public class Graph  implements IGraph   {
 			result.append("L" + i + ": (");
 			for (int j = 0; j < path.getLoop().length; j++) {
 				if (j == path.getLoop().length - 1)
-					result.append(nodesId[path.getLoop()[j]] + ") Gain="+path.getGainInt());
+					result.append(nodesId[path.getLoop()[j]] + ") Gain=" + path.getGainInt());
 				else
 					result.append(nodesId[path.getLoop()[j]] + ", ");
 
@@ -176,35 +167,38 @@ public class Graph  implements IGraph   {
 
 				int binary = 0, stacki = stack.size() - 1;
 				temp.push(child);
+				boolean isValidLoop = true;
 				binary = binary + (1 << child);
 				while (stack.get(stacki) != child) {
+					if (stack.get(stacki) < child) {
+						isValidLoop = false;
+						break;
+					}
+
 					binary = binary + (1 << stack.get(stacki));
 					temp.push(stack.get(stacki));
 					stacki--;
 
 					// System.out.print(loop[i]+" ");
 				}
-				temp.push(stack.get(stacki));
-				// System.out.println();
-				int i;
-				for (i = 0; i < loopsinbinary.size(); i++) {
-					if ((loopsinbinary.get(i) & binary) == (loopsinbinary.get(i) | binary))
-						break;
-				}
-				Integer[] loop = new Integer[temp.size()];
-				// System.out.println(temp.size());
-				for (int j = 0; j < temp.size(); j++) {
-					loop[j] = temp.get(temp.size() - j - 1);
-					// System.out.println(loop[j]+" j");
-				}
-				if (i == loopsinbinary.size()) {
+				if (isValidLoop) {
+					temp.push(stack.get(stacki));
+					// System.out.println();
+					Integer[] loop = new Integer[temp.size()];
+					// System.out.println(temp.size());
+					for (int j = 0; j < temp.size(); j++) {
+						loop[j] = temp.get(temp.size() - j - 1);
+						// System.out.println(loop[j]+" j");
+					}
 					loops.add(new Path(loop, binary, nodes, loops.size()));
 					nonTouchedLoops.add(new Path(loop, binary, nodes, nonTouchedLoops.size()));
 					loopsinbinary.add(binary);
-					delta-=loops.get(loops.size()-1).getGainInt();
+					delta -= loops.get(loops.size() - 1).getGainInt();
+					
 				}
 				child = nodes[x].getNextunvisitedChild();
 				continue;
+					
 
 			}
 			nodes[child].SetVisited();
@@ -301,10 +295,10 @@ public class Graph  implements IGraph   {
 				if (nonTouchedLoops.get(j).isAdditive(temp.getLoops(), temp.getBitnodes())) {
 					Path newLoop = new Path(nonTouchedLoops.get(j), temp);
 					nonTouchedLoops.add(i + 1, newLoop);
-					if(newLoop.getBitnodes().size()%2==0)
-						delta+=newLoop.getGainInt();
+					if (newLoop.getBitnodes().size() % 2 == 0)
+						delta += newLoop.getGainInt();
 					else
-						delta-=newLoop.getGainInt();
+						delta -= newLoop.getGainInt();
 					j++;
 				}
 
